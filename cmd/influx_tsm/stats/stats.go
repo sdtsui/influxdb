90d511bd5cab4781743dad85698ff4c684c399e4
sdtsui <szehungdanieltsui@gmail.com>
2016-09-11 15:34:15 -0700

    Finishes linting influx_tsm

--- cmd/influx_tsm/stats/stats.go
+++ cmd/influx_tsm/stats/stats.go
@@ -18,30 +18,37 @@ type Stats struct {
 	TotalTime       time.Duration
 }
 
+// AddPointsRead adds read points
 func (s *Stats) AddPointsRead(n int) {
 	atomic.AddUint64(&s.PointsRead, uint64(n))
 }
 
+// AddPointsWritten adds written points
 func (s *Stats) AddPointsWritten(n int) {
 	atomic.AddUint64(&s.PointsWritten, uint64(n))
 }
 
+// AddTSMBytes adds written TSM Bytes
 func (s *Stats) AddTSMBytes(n uint32) {
 	atomic.AddUint64(&s.TsmBytesWritten, uint64(n))
 }
 
+// IncrTSMFileCount adds the count of TSM files created
 func (s *Stats) IncrTSMFileCount() {
 	atomic.AddUint64(&s.TsmFilesCreated, 1)
 }
 
+// IncrNaN adds filtered NaNs
 func (s *Stats) IncrNaN() {
 	atomic.AddUint64(&s.NanFiltered, 1)
 }
 
+// IncrInf adds filtered Infs
 func (s *Stats) IncrInf() {
 	atomic.AddUint64(&s.InfFiltered, 1)
 }
 
+// IncrFiltered adds filtered fields
 func (s *Stats) IncrFiltered() {
 	atomic.AddUint64(&s.FieldsFiltered, 1)
 }
